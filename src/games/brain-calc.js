54e92f0068cb3d1f774667108f243a4d61cdac86
Anton Rehemae <rehemtoha@gmail.com>
2018-10-05 22:17:15 +0300

    fixBags

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,35 +1,29 @@
 import startGame from '../engineOfGame';
 import { getRandomNum } from '../functionAndData';
 
+const selectTest = () => {
+  const num1 = getRandomNum(0, 10);
+  const num2 = getRandomNum(0, 10);
+  const amountOperations = 3;
+  const selectedOperation = getRandomNum(1, amountOperations);
+  const test = {};
+  switch (selectedOperation) {
+    case 1:
+      test.question = `${num1} + ${num2}`;
+      test.answer = num1 + num2;
+      break;
+    case 2:
+      test.question = `${num1} - ${num2}`;
+      test.answer = num1 - num2;
+      break;
+    default:
+      test.question = `${num1} * ${num2}`;
+      test.answer = num1 * num2;
+      break;
+  }
+  return test;
+};
 const makeBrainGameCalc = () => {
-  const selectTest = () => {
-    const num1 = getRandomNum(0, 10);
-    const num2 = getRandomNum(0, 10);
-    const amountOperations = 3;
-    const selectedOperation = getRandomNum(1, amountOperations);
-    let obj = {};
-    switch (selectedOperation) {
-      case 1:
-        obj = {
-          question: `${num1} + ${num2}`,
-          answer: num1 + num2,
-        };
-        break;
-      case 2:
-        obj = {
-          question: `${num1} - ${num2}`,
-          answer: num1 - num2,
-        };
-        break;
-      default:
-        obj = {
-          question: `${num1} * ${num2}`,
-          answer: num1 * num2,
-        };
-        break;
-    }
-    return obj;
-  };
   const rules = 'What is the result of the expression?';
   return startGame(selectTest, rules);
 };
