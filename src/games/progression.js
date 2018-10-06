1475c7e0384a9ebae1c0a6a82eacc429b7430eea
Anton Rehemae <rehemtoha@gmail.com>
2018-10-06 12:42:46 +0300

    without complexity

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,12 +2,13 @@ import startGame from '../engineOfGame';
 import getRandomNum from '../utils';
 
 const maxLength = 10;
+const generateProgression = (start, step, acc = []) => {
+  if (acc.length === maxLength) return acc;
+  const newList = acc.concat([start]);
+  return generateProgression(start + step, step, newList);
+};
+
 const makeTest = () => {
-  const generateProgression = (start, step, acc = []) => {
-    if (acc.length === maxLength) return acc;
-    const newList = acc.concat([start]);
-    return generateProgression(start + step, step, newList);
-  };
   const progression = generateProgression(getRandomNum(1, 10), getRandomNum(1, 10));
   const hiddenElement = getRandomNum(0, progression.length);
   const answer = progression[hiddenElement];
