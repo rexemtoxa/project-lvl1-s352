33c5d61ed774c1efaa0516434d24acf5c7b109b3
Anton Rehemae <rehemtoha@gmail.com>
2018-10-07 12:42:15 +0300

    Cognitive Complexity

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,7 +3,6 @@ import getRandomNum from '../utils';
 
 const isPrime = (num) => {
   if (num < 2) return false;
-  if (num === 2) return true;
   const iter = (divisor) => {
     if (num % divisor === 0) return false;
     return divisor >= Math.sqrt(num) ? true : iter(divisor + 1);
