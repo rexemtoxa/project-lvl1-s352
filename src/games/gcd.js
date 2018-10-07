35dac01db4ff0cd45f8349376b05b19f2fc62c8b
Anton Rehemae <rehemtoha@gmail.com>
2018-10-07 11:57:56 +0300

    fix name

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,16 +1,16 @@
 import startGame from '../engineOfGame';
 import getRandomNum from '../utils';
 
-const GetBiggestDividor = (num1, num2) => {
+const getBiggestDividor = (num1, num2) => {
   if (num1 === 0) return num2;
-  return GetBiggestDividor(num2 % num1, num1);
+  return getBiggestDividor(num2 % num1, num1);
 };
 const getTest = () => {
   const num1 = getRandomNum(0, 100);
   const num2 = getRandomNum(1, 10);
   return {
     question: `${num1} ${num2}`,
-    answer: `${GetBiggestDividor(num1, num2)}`,
+    answer: `${getBiggestDividor(num1, num2)}`,
   };
 };
 
